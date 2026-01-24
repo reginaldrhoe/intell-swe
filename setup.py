ffff3cfed65e27323bf1842d339dfb1cc952aa6f
reginaldrhoe <reginald.rhoe@cstu.edu>
2026-01-24 15:31:42 -0800

    chore: bump version to 2.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rag_poc",
-    version="2.0.0",
+    version="2.4.0",
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
 )
